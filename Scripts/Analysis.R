591602d212ff0cd27fdbf8eba61f48c06ffcd20e
Julian Dietlmeier <julian@dietlmeiers.de>
2020-04-19 18:27:37 +0200

    Update 2020-04-19

--- Scripts/Analysis.R
+++ Scripts/Analysis.R
@@ -27,7 +27,7 @@ Germany_ts$Index <- xts(c(1:l), order.by = dates)
 tail(Germany_ts, 7)
 
 #Model Data
-phases <- c(4, 16, 14, 17, 7, 8, 19)
+phases <- c(4, 16, 14, 17, 7, 8, 22)
 Germany_ts_3pModel <- xts(WW_Data$Germany_Germany, order.by = dates)
 names(Germany_ts_3pModel) <- c("Data")
 Germany_ts_3pModel$Index <- xts(c(1:l), order.by = dates)
